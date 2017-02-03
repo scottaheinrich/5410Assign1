4053e952321a52b4f03f84b408e61d8ca9ffa1e6
Scott Heinrich <scottaheinrich@gmail.com>
2017-02-02 17:42:33 -0700

    Got variable in javascript working

--- js/loop.js
+++ js/loop.js
@@ -1,9 +1,32 @@
 document.getElementById("addEvent").onclick = function(){
-  //document.getElementById("output").innerHTML = 'Hey';
-  
+  //document.getElementById("output").innerHTML += 'Hey'+"<br/>";
+  var args = [document.getElementById("name").value,document.getElementById("interval").value,document.getElementById("times").value];
+  var time = Date.now();
+  gameLoop(args);
 };
 
-function doStuff(variable){
-  var myVar = variable;
-  return myVar;
+function gameLoop(args){
+  var name = args[0];
+  var interval = args[1];
+  var times = args[2];
+  var prevTime = Date.now();
+
+
+  document.getElementById("output").innerHTML += name + "<br/>";
+  document.getElementById("output").innerHTML += interval + "<br/>";
+  document.getElementById("output").innerHTML += times + "<br/>";
+  document.getElementById("output").innerHTML += prevTime + "<br/>";
+
+
+}
+
+function update(variable){
+
+
+
+}
+
+function render(variable){
+  var output = document.getElementById("output");
+  output.value += variable;
 }
