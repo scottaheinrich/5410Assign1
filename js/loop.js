111ed285f92fdb8321f208f3cca3ebd83bc9cf11
Scott Heinrich <scottaheinrich@gmail.com>
2017-02-02 17:01:59 -0700

    Got HTML styling done. CSS done. JS started

--- js/loop.js
+++ js/loop.js
@@ -1,1 +1,9 @@
+document.getElementById("addEvent").onclick = function(){
+  //document.getElementById("output").innerHTML = 'Hey';
+  
+};
 
+function doStuff(variable){
+  var myVar = variable;
+  return myVar;
+}
